38fcd4ffcfab5b38e298105026fec381f88adef8
pc-b <98758561+pc-b@users.noreply.github.com>
2026-08-15 00:20:47 -0400

    fix: remove empty import

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import {  } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
